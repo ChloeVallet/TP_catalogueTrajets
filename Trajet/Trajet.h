4743d2080cfcb6e8818037cccd0160057a69dd6d
chloe <chloemp.vallet@gmail.com>
2021-09-30 15:42:40 +0200

    Ajout 2 du travail de Jibril

---
+++ Trajet/Trajet.h
@@ -0,0 +1,78 @@
+/*************************************************************************
+                           Trajet  -  Classe abstraite permettant le polymophisme entre
+                           deux classes dérivées (TrajetSimple et TrajetComposé).
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) 2021 par Vallet Gharib
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Trajet> (fichier Trajet.h) ----------------
+#if ! defined ( TRAJET_H )
+#define TRAJET_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <iostream>
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Trajet>
+//  Classe abstraite permettant le polymophisme entre
+//  deux classes dérivées (TrajetSimple et TrajetComposé). Factorise les attributs communs à ces deux
+//  classes filles (villeDepart et villeArrive)
+//------------------------------------------------------------------------
+
+class Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Trajet ( const Trajet & unTrajet );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    virtual void Afficher()=0;
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Trajet (const char* villeD="", const char* villeA="" );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    char* villeDepart;
+    char* villeArrive;
+};
+
+//-------------------------------- Autres définitions dépendantes de <Trajet>
+
+#endif // TRAJET_H
+
